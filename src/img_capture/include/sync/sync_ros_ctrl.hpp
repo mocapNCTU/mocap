a2ab5998dc69b6da3f8f55f6f01301251e989547
BigDaddy <shooter2062424@gmail.com>
2017-02-26 05:44:24 +0800

    New SYNC by using ros message_filters

---
+++ src/img_capture/include/sync/sync_ros_ctrl.hpp
@@ -0,0 +1,294 @@
+//proper library to include
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <string>
+#include <ctype.h>
+
+//include ros usage
+#include "ros/ros.h"
+#include <opencv2/opencv.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+//include apriltag messages
+#include "apriltag/apriltags_all_msgs.hpp"
+
+using namespace std;
+using namespace cv;
+using namespace message_filters;
+
+//Ros part
+#define INFO img_capture::apriltagInfos::ConstPtr
+#define AI img_capture::apriltagInfos
+
+ros::Publisher infoBundle_publisher;
+ros::Subscriber oneSub;
+message_filters::Subscriber<img_capture::apriltagInfos>* sub;
+ros::NodeHandlePtr node;
+
+//default value
+const int CAM_NUMBER = 1;
+const int NO_CAM_NUM = -1;
+const bool APPROXIMATE = true;
+const bool EXACT = false;
+const bool DEFAULT_ALGORITHM = EXACT;
+
+//used parameter (some need to be set up)
+int cameraSyncNumber = NO_CAM_NUM;  //need use get parameter to setup
+string INFO_PREFIX, DEV_PREFIX;  //need use get parameter to setup
+string mynamePrefix;  //need use get parameter to setup
+bool useWhichAlgorithm = DEFAULT_ALGORITHM;  //need use get parameter to setup
+
+//simple id extract
+int extractID(string str)
+{ 
+	int pos = 0;
+	for(int i=0; i<str.length(); ++i)
+	{
+		if(isdigit(str[i]))
+		{
+			pos = i;
+			break;
+		}
+		if((i == (str.length()-1)) && (isdigit(str[i]) == false))
+		{
+			ROS_WARN("failed to extract id, exit with error");
+			exit(-1);
+		}
+	}
+	return stoi(str.substr(pos));
+}
+
+typedef class SYNC_SEAT
+{
+private:
+	img_capture::apriltagInfos** seats;
+	int seqNum;
+	int seatRemain;
+
+public:
+	//constructor
+	SYNC_SEAT()
+	{
+		seatRemain = cameraSyncNumber;
+		seqNum = -1;
+		seats = new img_capture::apriltagInfos*[cameraSyncNumber];
+		for(int i=0; i<cameraSyncNumber; ++i)
+		{
+			seats[i] = nullptr;
+		}
+	}
+	//destructor
+	~SYNC_SEAT()
+	{
+		if(seats != nullptr)
+		{
+			this->release();
+		}
+	}
+
+	//get seqNum
+	int getSeqNum()
+	{
+		return seqNum;
+	}
+
+	void setSeqNum(int val)
+	{
+		this->seqNum = val;
+	}
+
+	//all signal fit successfully
+	bool isSync()
+	{
+		return seatRemain == 0;	
+	}
+
+	//TODO should be called after "isSync()"
+	img_capture::apriltagInfosBundle* genInfoBundle()
+	{
+		img_capture::apriltagInfosBundle* ret = new img_capture::apriltagInfosBundle();
+		ret->seq = seqNum;
+		for(int i=0; i<cameraSyncNumber; ++i)
+		{
+	 		ret->bundle.push_back(*seats[i]);
+		}	
+		return ret;
+	}
+
+	//smart genInfoBundle
+	img_capture::apriltagInfosBundle* genInfoBundleSafe()
+	{
+		if(this->isSync())
+		{
+			return this->genInfoBundle();
+		}
+		
+		return nullptr;
+	}
+
+	//sit in
+	bool sitIn(const img_capture::apriltagInfos::ConstPtr& msg)
+	{
+		string str = msg->header.frame_id;
+		int pos = 0;
+		for(int i=0; i<msg->header.frame_id.length(); ++i)
+		{
+			if(isdigit(str[i]))
+			{
+				pos = i;
+				break;
+			}
+			if((i == (msg->header.frame_id.length()-1)) && (isdigit(str[i]) == false))
+			{
+				ROS_WARN("cannot recognize camera id, exit with error");
+				exit(-1);
+			}
+		}
+		int id = stoi(str.substr(pos));
+		if(id < cameraSyncNumber && seats[id] == nullptr)
+		{
+			seats[id] = new img_capture::apriltagInfos(*msg);
+			--seatRemain;
+			return true;
+		}
+		return false;
+	}
+
+	bool sitIn(img_capture::apriltagInfos* msg, int id)
+	{
+		if((id < cameraSyncNumber) && (seats[id] == nullptr))
+		{
+			seats[id] = new img_capture::apriltagInfos(*msg);
+			--seatRemain;
+			return true;
+		}
+		return false;
+	}
+
+	bool sitIn_shallow(img_capture::apriltagInfos* msg, int id)
+	{
+		if(id < cameraSyncNumber)
+		{
+			seats[id] = msg;
+			--seatRemain;
+			return true;
+		}
+		return false;
+	}	
+
+	bool sitIn(const img_capture::apriltagInfos::ConstPtr& msg, int id)
+	{
+		if(id < cameraSyncNumber)
+		{
+			seats[id] = new img_capture::apriltagInfos(*msg);
+			--seatRemain;
+			return true;	
+		}
+		return false;
+	}
+	
+	img_capture::apriltagInfos** getData()
+	{
+		return seats;
+	}
+
+	//TODO : may error by using none correct delete
+	void release()
+	{
+		for(int i=0; i<cameraSyncNumber; ++i)
+		{
+			delete seats[i];
+		}
+		delete[] seats;
+	}
+
+	void reset()
+	{
+		seatRemain = cameraSyncNumber;
+		seqNum = -1;
+		seats = new img_capture::apriltagInfos*[cameraSyncNumber];
+		for(int i=0; i<cameraSyncNumber; ++i)
+		{
+			seats[i] = nullptr;
+		}	
+	}
+
+	void reValid()
+	{
+		for(int i=0; i<cameraSyncNumber; ++i)
+		{
+			delete seats[i];
+			seats[i] = nullptr;
+		}
+		seatRemain = cameraSyncNumber;
+		seqNum = -1;
+	}
+
+	static SYNC_SEAT* create()
+	{
+		if(cameraSyncNumber != NO_CAM_NUM)
+		{
+			return new SYNC_SEAT();
+		}
+		else
+		{
+			return NULL;
+		}
+	}		
+} Seats; 
+
+
+//some definition
+const int SYNC_QUEUE_SIZE = 10;
+
+typedef sync_policies::ApproximateTime<AI, AI> ASP2;
+typedef sync_policies::ApproximateTime<AI, AI, AI> ASP3;
+typedef sync_policies::ApproximateTime<AI, AI, AI, AI> ASP4;
+typedef sync_policies::ApproximateTime<AI, AI, AI, AI, AI> ASP5;
+typedef sync_policies::ApproximateTime<AI, AI, AI, AI, AI, AI> ASP6;
+typedef sync_policies::ApproximateTime<AI, AI, AI, AI, AI, AI, AI> ASP7;
+typedef sync_policies::ApproximateTime<AI, AI, AI, AI, AI, AI, AI, AI> ASP8;
+typedef sync_policies::ApproximateTime<AI, AI, AI, AI, AI, AI, AI, AI, AI> ASP9;
+
+typedef sync_policies::ExactTime<AI, AI> ESP2;
+typedef sync_policies::ExactTime<AI, AI, AI> ESP3;
+typedef sync_policies::ExactTime<AI, AI, AI, AI> ESP4;
+typedef sync_policies::ExactTime<AI, AI, AI, AI, AI> ESP5;
+typedef sync_policies::ExactTime<AI, AI, AI, AI, AI, AI> ESP6;
+typedef sync_policies::ExactTime<AI, AI, AI, AI, AI, AI, AI> ESP7;
+typedef sync_policies::ExactTime<AI, AI, AI, AI, AI, AI, AI, AI> ESP8;
+typedef sync_policies::ExactTime<AI, AI, AI, AI, AI, AI, AI, AI, AI> ESP9;
+
+void rcv_call1(const INFO&);
+void rcv_call2(const INFO&, const INFO&);
+void rcv_call3(const INFO&, const INFO&, const INFO&);
+void rcv_call4(const INFO&, const INFO&, const INFO&, const INFO&);
+void rcv_call5(const INFO&, const INFO&, const INFO&, const INFO&, const INFO&);
+void rcv_call6(const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&);
+void rcv_call7(const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&);
+void rcv_call8(const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&);
+void rcv_call9(const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&, const INFO&);
+
+void setupConnection();
+void getParameters();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/img_capture/src/synchronizer_ros_ctrl.cpp
@@ -0,0 +1,278 @@
+//include synchronizer function
+#include "sync/sync_ros_ctrl.hpp"
+
+int offset = 1;  //to avoid my laptop's webcam, use 0 if u don't have this problem
+int main(int argc, char **argv)
+{
+	//initial ros node
+	ros::init(argc, argv, "synchronizer_ros_ctrl");
+	node = boost::make_shared<ros::NodeHandle>();
+	
+	//check sync parameters
+	getParameters();
+    	
+	//setup ros connection with other nodes
+	setupConnection();
+
+	//ros time control
+	ros::spin();
+
+	return 0;
+}
+
+void getParameters()
+{
+	mynamePrefix = ros::this_node::getName() + "/";
+	node->param(mynamePrefix + "cam_num", cameraSyncNumber, NO_CAM_NUM);
+	node->param(mynamePrefix + "info_prefix", INFO_PREFIX, string(""));
+	node->param(mynamePrefix + "device_prefix", DEV_PREFIX, string(""));
+	node->param(mynamePrefix + "use_algorithm", useWhichAlgorithm, DEFAULT_ALGORITHM);
+	if(useWhichAlgorithm == EXACT)
+	{
+		ROS_INFO("Use EXACT sync!");
+	}
+	else
+	{
+		ROS_INFO("Use APPROXIMATE sync!");
+	}
+
+	if((INFO_PREFIX.empty()) || (cameraSyncNumber == NO_CAM_NUM) || (DEV_PREFIX.empty()))
+	{
+		ROS_WARN("cannot fetch 'Synchronizer' node's parameters, exit with error");
+		exit(-1);
+	}
+}
+
+void rcv_call1(const img_capture::apriltagInfos::ConstPtr& a)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call2(const INFO& a, const INFO& b)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call3(const INFO& a, const INFO& b, const INFO& c)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	msg->sitIn(c, 2);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call4(const INFO& a, const INFO& b, const INFO& c, const INFO& d)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	msg->sitIn(c, 2);
+	msg->sitIn(d, 3);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call5(const INFO& a, const INFO& b, const INFO& c, const INFO& d, const INFO& e)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	msg->sitIn(c, 2);
+	msg->sitIn(d, 3);
+	msg->sitIn(e, 4);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call6(const INFO& a, const INFO& b, const INFO& c, const INFO& d, const INFO& e, const INFO& f)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	msg->sitIn(c, 2);
+	msg->sitIn(d, 3);
+	msg->sitIn(e, 4);
+	msg->sitIn(f, 5);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call7(const INFO& a, const INFO& b, const INFO& c, const INFO& d, const INFO& e, const INFO& f, const INFO& g)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	msg->sitIn(c, 2);
+	msg->sitIn(d, 3);
+	msg->sitIn(e, 4);
+	msg->sitIn(f, 5);
+	msg->sitIn(g, 6);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call8(const INFO& a, const INFO& b, const INFO& c, const INFO& d, const INFO& e, const INFO& f, const INFO& g, const INFO& h)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	msg->sitIn(c, 2);
+	msg->sitIn(d, 3);
+	msg->sitIn(e, 4);
+	msg->sitIn(f, 5);
+	msg->sitIn(g, 6);
+	msg->sitIn(h, 7);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+void rcv_call9(const INFO& a, const INFO& b, const INFO& c, const INFO& d, const INFO& e, const INFO& f, const INFO& g, const INFO& h, const INFO& i)
+{
+	Seats* msg = Seats::create();
+	msg->sitIn(a, 0);
+	msg->sitIn(b, 1);
+	msg->sitIn(c, 2);
+	msg->sitIn(d, 3);
+	msg->sitIn(e, 4);
+	msg->sitIn(f, 5);
+	msg->sitIn(g, 6);
+	msg->sitIn(h, 7);
+	msg->sitIn(i, 8);
+	infoBundle_publisher.publish(*(msg->genInfoBundle()));
+	delete msg;
+}
+
+
+void setupConnection()
+{
+	//build up publisher
+	infoBundle_publisher = node->advertise<img_capture::apriltagInfosBundle>("/apriltags_sync_info", 1000);
+
+	//build up subscribers
+	if(cameraSyncNumber >= 2)
+	{
+		sub = new message_filters::Subscriber<AI>[cameraSyncNumber];
+		for(int i=0; i<cameraSyncNumber; ++i)
+		{
+			sub[i].subscribe(*node, DEV_PREFIX + to_string(i+offset) + "/" + INFO_PREFIX, 10);
+		}
+	
+		//setup subscribe sync
+		switch(cameraSyncNumber)
+		{
+			case 1:
+					break;
+			case 2:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new message_filters::Synchronizer<ESP2>(ESP2(SYNC_QUEUE_SIZE), sub[0], sub[1]);
+						sync->registerCallback(boost::bind(&rcv_call2, _1, _2));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP2>(ASP2(SYNC_QUEUE_SIZE), sub[0], sub[1]);
+						sync->registerCallback(boost::bind(&rcv_call2, _1, _2));
+					}
+					break;
+			case 3:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new Synchronizer<ESP3>(ESP3(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2]);
+						sync->registerCallback(boost::bind(&rcv_call3, _1, _2, _3));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP3>(ASP3(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2]);
+						sync->registerCallback(boost::bind(&rcv_call3, _1, _2, _3));
+					}
+					break;
+			case 4:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new Synchronizer<ESP4>(ESP4(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3]);
+						sync->registerCallback(boost::bind(&rcv_call4, _1, _2, _3, _4));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP4>(ASP4(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3]);
+						sync->registerCallback(boost::bind(&rcv_call4, _1, _2, _3, _4));
+					}
+					break;
+			case 5:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new Synchronizer<ESP5>(ESP5(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4]);
+						sync->registerCallback(boost::bind(&rcv_call5, _1, _2, _3, _4, _5));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP5>(ASP5(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4]);
+						sync->registerCallback(boost::bind(&rcv_call5, _1, _2, _3, _4, _5));
+					}
+					break;
+			case 6:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new Synchronizer<ESP6>(ESP6(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5]);
+						sync->registerCallback(boost::bind(&rcv_call6, _1, _2, _3, _4, _5, _6));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP6>(ASP6(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5]);
+						sync->registerCallback(boost::bind(&rcv_call6, _1, _2, _3, _4, _5, _6));
+					}
+					break;
+			case 7:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new Synchronizer<ESP7>(ESP7(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6]);
+						sync->registerCallback(boost::bind(&rcv_call7, _1, _2, _3, _4, _5, _6, _7));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP7>(ASP7(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6]);
+						sync->registerCallback(boost::bind(&rcv_call7, _1, _2, _3, _4, _5, _6, _7));
+					}
+					break;
+			case 8:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new Synchronizer<ESP8>(ESP8(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6], sub[7]);
+						sync->registerCallback(boost::bind(&rcv_call8, _1, _2, _3, _4, _5, _6, _7, _8));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP8>(ASP8(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6], sub[7]);
+						sync->registerCallback(boost::bind(&rcv_call8, _1, _2, _3, _4, _5, _6, _7, _8));
+					}
+					break;
+			case 9:
+					if(!useWhichAlgorithm)
+					{
+						auto sync = new Synchronizer<ESP9>(ESP9(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6], sub[7], sub[8]);
+						sync->registerCallback(boost::bind(&rcv_call9, _1, _2, _3, _4, _5, _6, _7, _8, _9));
+					}
+					else
+					{
+						auto sync = new Synchronizer<ASP9>(ASP9(SYNC_QUEUE_SIZE), sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6], sub[7], sub[8]);
+						sync->registerCallback(boost::bind(&rcv_call9, _1, _2, _3, _4, _5, _6, _7, _8, _9));
+					}
+					break;
+			default:
+					ROS_WARN("Exceed max supported of the sync channel, exit with error");
+		}
+	}
+	else
+	{
+		oneSub = node->subscribe(DEV_PREFIX + to_string(0+offset) + "/" + INFO_PREFIX, 100, rcv_call1);
+	}
+}
